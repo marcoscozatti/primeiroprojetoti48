7163add5cff6127ac012ac4218343083671d1bf1
Marcos Cozatti <marcoscozatti@gmail.com>
2025-11-10 17:05:26 -0300

    Criamos um botão com um label na tela

--- PrimeiroProjetoTI48/Form1.cs
+++ PrimeiroProjetoTI48/Form1.cs
@@ -16,5 +16,10 @@ namespace PrimeiroProjetoTI48
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Olá turma TI48";
+        }
     }
 }
