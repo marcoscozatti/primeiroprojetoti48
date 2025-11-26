e5e9ba82c5e110d0ec3f941312d94bb947518cb2
Marcos Cozatti <marcoscozatti@gmail.com>
2025-11-26 13:38:58 -0300

    bkp de arquivos do dia anterior

---
+++ PrimeiroProjetoTI48/Connection.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+public class Connection
+{
+    private SqlConnection conn;
+
+    private string connectionString = @"Server=SEU_SERVIDOR;Database=AgendaDB;Trusted_Connection=True;";
+
+    public SqlConnection Connect()
+    {
+        conn = new SqlConnection(connectionString);
+
+        if (conn.State == ConnectionState.Closed)
+            conn.Open();
+
+        return conn;
+    }
+
+    public void Disconnect()
+    {
+        if (conn != null && conn.State == ConnectionState.Open)
+            conn.Close();
+    }
+}
